45196b3b6d5e49fd0b04bcc79ec9191ad3df9d53
Sergio Gtz <sergio.oscar.gb@gmail.com>
2018-03-24 14:33:18 -0600

    Use bucket name and object key from codepipeline, and notify when codepipeline job done

--- upload-porfolio-lambda.py
+++ upload-porfolio-lambda.py
@@ -7,14 +7,32 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-west-2:647579903866:portfolio-deployPortfolio-topic')
 
+    # Follwing code is executed when run this program manually
+    location : {
+        "bucketName" : 'portfoliobuild.sergiogtz.info',
+        "objectKey" : 'portfoliobuild.zip'
+    }
+
     try:
         s3 = boto3.resource('s3')
+
+        job = event.get("CodePipeline.job")
+
+        # Follwing if statment executes code when CodePipeline
+        # has been activated
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyBuildApp"
+                    location == artifact["locatio"]["s3Location"]
+
+        print("Building portfolio from: " + str[location])
+
         portfolio_bucket = s3.Bucket('portfolio.sergiogtz.info')
-        build_bucket = s3.Bucket('portfoliobuild.sergiogtz.info')
+        build_bucket = s3.Bucket(location["bucketName"])
 
         portfolio_zip = io.BytesIO()
 
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -24,6 +42,10 @@ def lambda_handler(event, context):
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
         topic.publish(Subject="Portfolio deployed", Message="Portfolio deployed successfully!.")
+
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId = job["id"])
     except:
         topic.publish(Subject="Portfolio deploy failed", Message="The portfolio was not deployed successfully!.")
         raise
